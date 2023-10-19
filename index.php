1d79d0fddb9df88531c054ad39df59cf34b1e736
rpsobolewski <rpsobolewski@gmail.com>
2023-10-19 17:04:12 +0200

    add 2 genres

--- index.php
+++ index.php
@@ -6,27 +6,32 @@ class Movie
     public $title;
     public $director;
     public $year;
+    public $genres;
 
 
-    public function __construct($title, $director, $year)
+    public function __construct($title, $director, $year, ...$genres)
     {
         $this->title = $title;
         $this->director = $director;
         $this->year = $year;
+        $this->genres = $genres;
     }
 
 
     public function getMovieInfo()
     {
-        return "Titolo: {$this->title}, Regista: {$this->director}, Anno: {$this->year}";
+        $genreString = implode(', ', $this->genres);
+        return "Titolo: {$this->title}, Regista: {$this->director}, Anno: {$this->year}, Generi: {$genreString}";
     }
 }
 
 
+
+
 $movies = [];
 
-$movies[] = new Movie("Kill Bill: Volume 1", "Quentin Tarantino", 2003);
-$movies[] = new Movie("The Matrix", "the Wachowskis", 1999);
+$movies[] = new Movie("Kill Bill: Volume 1", "Quentin Tarantino", 2003, "Azione", "Thriller");
+$movies[] = new Movie("The Matrix", "the Wachowskis", 1999, "Fantascienza", "Azione");
 
 foreach ($movies as $movie) {
     echo $movie->getMovieInfo() . "<br>";
